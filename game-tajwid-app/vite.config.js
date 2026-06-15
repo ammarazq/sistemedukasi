160d32f4a54ea61d47de6632112d1e598fd10f5d
ammarazq <146455979+ammarazq@users.noreply.github.com>
2026-06-16 00:45:06 +0700

    coba buat pwa (#5)
    * nambah komen aowkokw
    
    * ngurangin guide
    
    * penambahan mad dhomah
    
    * perbaikan skor
    
    * memasukkan audio semuanya kecuali audio gameplay
    
    * audio gameplay
    
    * merubah homescreen
    
    * menambahkan audio
    
    * mencoba membuat pwa
    
    ---------
    
    Co-authored-by: Arddd <128204076+Arddiand761@users.noreply.github.com>

--- game-tajwid-app/vite.config.js
+++ game-tajwid-app/vite.config.js
@@ -1,7 +1,65 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'audio/*.mp3',
+        'favicon.ico',
+        'apple-touch-icon.png',
+      ],
+
+      manifest: {
+        name: 'GameLand',
+        short_name: 'GameLand',
+
+        description:
+          'Game edukasi belajar harakat hijaiyah',
+
+        theme_color: '#2db891',
+
+        background_color: '#ffffff',
+
+        display: 'standalone',
+
+        orientation: 'portrait',
+
+        start_url: '/',
+
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
